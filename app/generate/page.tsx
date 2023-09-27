b3427d6ba6a42646d6350c22b2acfbb1e74d5024
fal3n-4ngel <adiadithyakrishnan@gmail.com>
2023-09-27 14:55:05 +0530

    update : rhon's wish
    Co-authored-by: Ritin George <aurora-0025@users.noreply.github.com>
    Co-authored-by: vinu b kurup <Hipster16@users.noreply.github.com>

--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -14,18 +14,80 @@ import {
 import { Calendar as CalendarIcon } from "lucide-react"
 import { cn } from "@/lib/utils"
 import { format } from "date-fns"
-import { ChangeEvent, FormEvent, useEffect, useState } from "react"
+import { ChangeEvent, FormEvent, useEffect, useRef, useState } from "react"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import jsPDF from "jspdf"
+""
 
+var pdf = new jsPDF("l","pt","a4");
+function Download(){
+  pdf.save("certificate.pdf");
+  
+}
 export default function Component() {
   const [date, setDate] = useState<Date>()
   const [orgName, setOrgname] = useState<string>("");
   const [eventName, setEventname] = useState<string>("");
   const [recipientName, setRecipientName] = useState<string>("");
   const [aadhaarId, setAadhaarId] = useState<string>("");
+  const [certificateImage, setCertificateImage] = useState<string>("");
+  const [generating, setGenerating] = useState(false);
+
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  function generateCertificate() {
+    setGenerating(true);
+    const canvas: HTMLCanvasElement | null = canvasRef.current!;
+    canvas.width = 800;
+    canvas.height = 565.5;
+    const ctx = canvas.getContext('2d')!;
+    const bg = new Image();
+    bg.setAttribute('crossorigin', 'anonymous');
+    bg.src = "https://cdn.discordapp.com/attachments/946819313342500914/1156447200839204935/of_participation.png?ex=651500e2&is=6513af62&hm=dd10af8d521374fe8f5a8e7cf4a13f35dc40ac5df17fecfd6d0be02966d7e334&";
+    bg.onload = () => {
+      ctx.drawImage(bg, 0, 0, canvas.width, canvas.height);
+      const qr = new Image();
+      qr.src = "https://cdn.discordapp.com/attachments/930377076324839474/1156480301338476586/default-preview-qr.png?ex=65151fb6&is=6513ce36&hm=d8ab1cb327a08ffc188bcea89fd2fd548a84c43d60d857082b75cc3ce66bba32&";
+      qr.setAttribute('crossorigin', 'anonymous');
+      qr.onload = () => {
+        ctx.drawImage(qr,82, 80, 60, 60);
+        ctx.font = "30px Comic Sans MS";
+        ctx.textAlign = "center";
+        ctx.fillText(recipientName, canvas.width/2, canvas.height/2-15);
+        ctx.font = "17px Comic Sans MS";
+        ctx.fillText("has successfully completed a training", canvas.width/2, canvas.height/2+40)
+        ctx.fillText(`programme on ${eventName}`, canvas.width/2, canvas.height/2+65)
+        ctx.fillText(`conducted by ${orgName}`, canvas.width/2, canvas.height/2+90)
+  
+        var imgData = canvas.toDataURL("image/jpeg", 1.0);
+        setCertificateImage(imgData);
+        pdf.addImage(imgData, 'JPEG', 0, 0, 842, 595);
+        setGenerating(false);
+      }
+      //  pdf.setProperties({
+      //   "title":"ewfkukewbczxtqtu313jk8274hj"
+      //  })
+       
+
+    }
+
+  }
+
+
+
+
 
   return (
     <div className="flex items-center justify-center h-screen bg-black ">
-
+      <canvas hidden ref={canvasRef}>  </canvas>;
       <Card>
         <CardContent>
           <div className="space-y-8 p-10">
@@ -42,29 +104,29 @@ export default function Component() {
                   <Input id="org-name" placeholder="Enter the organistation name" value={orgName} onChange={(e: FormEvent<HTMLInputElement>) => {
                     e.preventDefault();
                     setOrgname(e.currentTarget.value);
-                    }} type="text"/>
+                  }} type="text" />
                 </div>
                 <div className="space-y-2">
                   <Label htmlFor="event-name">Event Name</Label>
                   <Input id="event-name" placeholder="Enter event name" value={eventName} onChange={(e: FormEvent<HTMLInputElement>) => {
                     e.preventDefault();
                     setEventname(e.currentTarget.value);
-                    }} />
+                  }} />
                 </div>
               </div>
               <div className="space-y-2">
                 <Label htmlFor="name">Name</Label>
                 <Input id="name" placeholder="Enter the recipient name" type="text" value={recipientName} onChange={(e: FormEvent<HTMLInputElement>) => {
-                    e.preventDefault();
-                    setRecipientName(e.currentTarget.value);
-                    }}/>
+                  e.preventDefault();
+                  setRecipientName(e.currentTarget.value);
+                }} />
               </div>
               <div className="space-y-2">
                 <Label>Aadhar id</Label>
                 <Input id="aadhar" placeholder="Enter the aadhar id" type="text" value={aadhaarId} onChange={(e: FormEvent<HTMLInputElement>) => {
-                    e.preventDefault();
-                    setAadhaarId(e.currentTarget.value);
-                    }}/>
+                  e.preventDefault();
+                  setAadhaarId(e.currentTarget.value);
+                }} />
               </div>
               <div className="flex flex-col space-y-2">
                 <Label>Issue Date</Label>
@@ -91,9 +153,28 @@ export default function Component() {
                   </PopoverContent>
                 </Popover>
               </div>
-              <Button className="bg-gray-800 text-white" type="submit" >
-                Generate Certificate
-              </Button>
+
+
+              <Dialog open={generating}>
+                <DialogTrigger >
+                  <Button variant="outline" onClick={generateCertificate} className="bg-gray-800 text-white" type="submit">Generate Certificate</Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                  <DialogHeader>
+                    <DialogTitle>Certificate Generated</DialogTitle>
+                    <DialogDescription>
+                      Make changes to your profile here. Click save when youre done.
+                    </DialogDescription>
+                  </DialogHeader>
+                  <div className="grid gap-4 py-4">
+                      <img width={100} height={100} src={certificateImage} alt="" crossOrigin="anonymous"  />
+                  </div>
+                  <DialogFooter>
+                    <Button type="submit" onClick={Download}>Download</Button>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
+
             </div>
           </div>
         </CardContent>
--- app/verify/[id]/page.tsx
+++ app/verify/[id]/page.tsx
@@ -7,24 +7,44 @@ let loading = false;
 let verified = true;
 
 
-function Verify() {
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
 
+function Verify() {
+    const certDetails = {
+        name: "RIYANNA MARIA ABISON",
+        event: "SWAYAM",
+        type: "WEB DEVELOPMENT",
+        qrLink: "https://cdn.discordapp.com/attachments/946819313342500914/1156447200839204935/of_participation.png?ex=651500e2&is=6513af62&hm=dd10af8d521374fe8f5a8e7cf4a13f35dc40ac5df17fecfd6d0be02966d7e334&"
+    }
 
     return (
 
         <div className='w-full min-h-screen bg-white text-black'>
             {loading ? <Loading /> : <div>
-                {verified ? <div className="flex w-full h-full p-10 justify-center items-center">
-                    <Image
-                        src="/verified.gif"
-                        width={70}
-                        height={70}
-                        alt="Picture of the author"
-                        className="p-2"
-                    />
-                    <div className="text-[60px] text-black text-center">Your Certificate is verified</div>
-                    <div></div>
-                </div> : <div></div>}
+                {verified ?
+                    <div>
+                        <div className="flex md:flex-row flex-col w-full h-full p-10 justify-center items-center">
+                            <Image
+                                src="/verified.gif"
+                                width={70}
+                                height={70}
+                                alt="Picture of the author"
+                                className="p-2"
+                            />
+                            <div className="text-[30px] md:text-[60px] text-black text-center">Your Certificate is verified</div>
+
+                        </div>
+                        <div className="flex w-full justify-center">
+
+                        </div>
+                    </div> : <div></div>}
             </div>}
         </div>
     )
