964a0e16cd39626deb00fd6921cd3d3cb2f5dbb7
Ritin George <ritingeorge.d@gmail.com>
2023-09-28 19:11:23 +0530

    feat: allow users without metamask to use "Infura"

--- app/verify/[id]/page.tsx
+++ app/verify/[id]/page.tsx
@@ -2,60 +2,49 @@
 import Loading from "@/components/Loading";
 import Image from "next/image";
 import React, { useEffect, useRef, useState } from "react";
-import CertificateVerification from "@/src/artifacts/contracts/CertificateVerification.sol/CertificateVerification.json";
-
-import { useRouter } from 'next/navigation'
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-  } from "@/components/ui/dialog"
+import CertificateVerification from "@/artifacts/contracts/CertificateVerify.sol/CertificateVerification.json";
 import { ethers } from "ethers";
 
 function Verify({ params }: { params: { id: string } }) {
     const [verified, setVerified] = useState(false);
     const [loading, setLoading] = useState(true);
     const [certImg, setCertImg] = useState("");
-    const contractAddress = "0x76a737Dd55906b8D679C6826a873Bf9FCc8e0aB2";
-    const router = useRouter()
+    const contractAddress = "0x13689bC9Ca59811178330cD3f5dB9a831706472e";
+    let provider = null;
 
     useEffect(() => {
-
-        console.log(params.id);
-
-
       async function verifyCert() {
+        if (window.ethereum == null) {
 
-        const connectWallet = async () => {
-            const accounts= await window.ethereum.request({method: 'eth_requestAccounts'});
-            console.log(accounts)
-            }
-
-        await connectWallet();
+            // If MetaMask is not installed, we use the default provider,
+            // which is backed by a variety of third-party services (such
+            // as INFURA). They do not have private keys installed so are
+            // only have read-only access
+            console.log("MetaMask not installed; using read-only defaults")
+            provider = new ethers.JsonRpcProvider(`https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API_KEY}`);
+        } else {
+            provider = new ethers.BrowserProvider(window.ethereum)
+        }
         setLoading(true);
-        const provider = new ethers.providers.Web3Provider(
-            window?.ethereum
-        );
         
-        const signer = provider?.getSigner();
         const contract = new ethers.Contract(
             contractAddress,
             CertificateVerification.abi,
-            signer
+            provider
         );
 
         try {            
             const response = await contract.getCertificate(params.id);
-            console.log(response);
             console.log("response:", response);
             setCertImg(response.imgHash);
             setLoading(false);
             setVerified(true);
             console.log("verified");
         } catch (error) {
+            setLoading(false);
+            setVerified(false);
+        }
+        return () => {
             setLoading(false);
             setVerified(false);
         }
@@ -85,21 +74,7 @@ function Verify({ params }: { params: { id: string } }) {
                         <img src={certImg} width={500} height={500}/>
 
                         </div>
-                    </div> : <div><div className="flex md:flex-row flex-col w-full h-full p-10 justify-center items-center">
-                            <Image
-                                src="/verified.gif"
-                                width={70}
-                                height={70}
-                                alt="Picture of the author"
-                                className="p-2"
-                            />
-                            <div className="text-[30px] md:text-[60px] text-black text-center">Your Certificate is verified</div>
-                            
-                        </div>
-                        <div className="flex w-full justify-center">
-                        <img src={certImg} width={500} height={500}/>
-
-                        </div></div>}
+                    </div> : <div>This Certificate is invalid</div>}
             </div>}
         </div>
     )
