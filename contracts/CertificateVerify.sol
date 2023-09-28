a8a1c7cc9c54eb3af2cb04de0cf09fe783acf44d
Ritin George <ritingeorge.d@gmail.com>
2023-09-28 19:07:23 +0530

    feat: whitelist function in contract

--- contracts/CertificateVerify.sol
+++ contracts/CertificateVerify.sol
@@ -16,13 +16,39 @@ contract CertificateVerification {
     }
     mapping(string => Certificate) internal certificates;
     address payable public owner;
+    mapping(address => bool) public whitelist;
 
+    event IssuedCertificate(address indexed issuer, uint256 _issueDate);
+
+    /**
+     * @notice Add owner wallet to whitelist
+     */
     constructor() {
+        whitelist[msg.sender] = true;
         owner = payable(msg.sender);
     }
 
+    /**
+     * @notice Add wallet to whitelist
+     */
+    function addWhitelistWallet(address payable _wallet) public {
+        require(msg.sender == owner, "ONLY_OWNER_CAN_ADD");
+        whitelist[_wallet] = true;
+    }
+
+     /**
+     * @notice Remove wallet from whitelist
+     */
+    function removeWhitelistWallet(address payable _wallet) public {
+        delete whitelist[_wallet];
+    }
+
+    /**
+     * @notice Whitelisted wallets can issue certificate
+     */
     function issueCertificate(string memory _id, string memory _recipientName, string memory _recipientAadhaar, string memory _imgHash, uint256 _issueDate) public  {
-        require(msg.sender == owner, "Only the contract owner can issue certificates!");
+        require(whitelist[msg.sender], "NOT_IN_WHITELIST");
+        require(certificates[_id].creationDate == 0, "ALREADY_EXISTS");
         Certificate memory newCertificate = Certificate({
             issuer: msg.sender,
             recipientName: _recipientName,
@@ -31,12 +57,15 @@ contract CertificateVerification {
             issueDate: _issueDate,
             creationDate: block.timestamp
         });
-        require(certificates[_id].creationDate == 0, "certificate with that id already exists!");
         certificates[_id] = newCertificate;
+        emit IssuedCertificate(msg.sender, block.timestamp);
     }
 
+    /**
+     * @notice Retrieve certificate from blockchain
+     */
     function getCertificate(string memory _id) public view returns (Certificate memory) {
-        require(certificates[_id].creationDate != 0, "cannot find certificate with that id!");
+        require(certificates[_id].creationDate != 0, "CANNOT_FIND");
         return certificates[_id];
     }
 }
