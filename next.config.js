44b2f4b18b2a7ffdaef1e4cfdf45481432689286
Ritin George <ritingeorge.d@gmail.com>
2023-09-30 04:34:48 +0530

    fix: module parse error

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ const nextConfig = {
         domains: ["firebasestorage.googleapis.com"],
         formats: ["image/webp"]
     },
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        // Exclude the problematic file from being processed by webpack
+        config.module.rules.push({
+          test: /canvas\.node$/,
+          use: 'ignore-loader',
+        });
+    
+        return config;
+      },
 }
 
 module.exports = nextConfig
