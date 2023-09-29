06fc8b042a3ccbc41b6b9c54b142db6b2072688a
fal3n-4ngel <adiadithyakrishnan@gmail.com>
2023-09-29 08:31:36 +0530

    fix : build errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 "use client"
+
+import Link from "next/link"
+
+import React, { useEffect } from "react";
+import Router from 'next/router'
+import { Button } from "react-day-picker";
+
+
 export default function Home() {
   return (
     <main>
-      <div className='flex items-center justify-center w-full h-full min-h-screen'>
+      <div className='flex flex-col items-center justify-center w-full h-full min-h-screen'>
         <div className='text-[150px] text-center' >Smart Certify</div>
+       <a href="/generate"  className="flex items-center justify-center bg-black w-[150px] h-[60px] text-white  text-3xl text-center rounded-xl hover:bg-slate-900 hover:text-slate-400">Generate</a>
       </div>
     </main>
   )
